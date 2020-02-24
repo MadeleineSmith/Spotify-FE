c55093499f50a75b93f6c341bff53eb1c7eff9e0
Madeleine Smith <madeleinesmith222@gmail.com>
2020-02-24 22:44:37 +0000

    Now have two forms instead

--- src/components/CreateKillerPlaylist.vue
+++ src/components/CreateKillerPlaylist.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="hello">
-    <h1>CREATE KILLER PLAYLIST</h1>
+    <h1>Create your playlist</h1>
+
+    <form id="specific-date-form" @submit.prevent="formSubmitted($data)" >
+      <label>Select a date:</label>
+      
+      <input type="date" id="dateField"  v-model="formFields.date" min="1952-11-14" :max="maximumDate()">
+    
+      <input type="submit" value="Submit" :disabled="playlistCreationInProgress">
+      <font-awesome-icon icon="spinner" spin v-show="playlistCreationInProgress"></font-awesome-icon>
+    </form>
+
+    <hr>
 
     <form id="example-basic" @submit.prevent="formSubmitted($data)" >
-      <!-- <input type="text" placeholder="Playlist name" v-model="formFields.name" :disabled="playlistCreationInProgress"> -->
-      <label for="yearpicker">Select year:</label>
+    
+      <label for="yearpicker">Or randomise on a year:</label>
 
       <select name="yearpicker" id="yearpicker" v-model="formFields.year" :disabled="playlistCreationInProgress">
         <option v-for="n in getNumbers()" :value="n">{{ n }}</option>
@@ -47,6 +58,22 @@
 
     },
     methods: {
+      maximumDate() {
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd
+        } 
+        if(mm<10){
+            mm='0'+mm
+        } 
+
+        today = yyyy+'-'+mm+'-'+dd;
+
+        return today
+    },
       getNumbers() {
           const startingYear = 1952;
 
