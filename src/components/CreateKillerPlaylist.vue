b21912b0a31d5e31f8b550c72ef5af2b54fd9f4a
Madeleine Smith <madeleinesmith222@gmail.com>
2020-03-06 00:09:48 +0000

    Got a linter + vuetify works

--- src/components/CreateKillerPlaylist.vue
+++ src/components/CreateKillerPlaylist.vue
@@ -1,127 +1,159 @@
 <template>
   <div class="hello">
     <h1>Create your playlist</h1>
+    <v-row>
+      <v-col xs12 sm6 md4>
+        <form id="specific-date-form" @submit.prevent="specificDateformSubmitted()">
+          <label>Create a chart on:</label>
 
-    <form id="specific-date-form" @submit.prevent="specificDateformSubmitted()" >
-      <label>Create a chart on:</label>
-      
-      <input type="date" id="dateField"  v-model="formFields.date" min="1952-11-14" :max="maximumDate()">
-    
-      <input type="submit" value="Submit" :disabled="playlistCreationInProgress">
-      <font-awesome-icon icon="spinner" spin v-show="playlistCreationInProgress"></font-awesome-icon>
-    </form>
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="formFields.date"
+                label="Specific date"
+                prepend-icon="event"
+                readonly
+                v-on="on"
+              ></v-text-field>
+            </template>
 
-    <hr>
+            <v-date-picker
+              v-model="formFields.date"
+              @input="menu = false"
+              min="1952-11-14"
+              :max="maximumDate()"
+            ></v-date-picker>
+          </v-menu>
 
-    <form id="example-basic" @submit.prevent="minYearFormSubmitted()" >
-    
-      <label for="yearpicker">Randomise a chart since:</label>
+          <input type="submit" value="Submit" :disabled="playlistCreationInProgress" />
+          <font-awesome-icon icon="spinner" spin v-show="playlistCreationInProgress"></font-awesome-icon>
+        </form>
+      </v-col>
 
-      <input type="number" min=1952 :max="maximumNumYears()" v-model.number="formFields.minYear">
+      <v-col xs12 sm6 md4>
+        <form id="example-basic" @submit.prevent="minYearFormSubmitted()">
+          <label for="yearpicker">Randomise a chart since:</label>
 
-      <br>
-      <input type="submit" value="Submit" :disabled="playlistCreationInProgress">
-      <font-awesome-icon icon="spinner" spin v-show="playlistCreationInProgress"></font-awesome-icon>
-    </form>
+          <input
+            type="number"
+            min="1952"
+            :max="maximumNumYears()"
+            v-model.number="formFields.minYear"
+          />
 
+          <br />
+          <input type="submit" value="Submit" :disabled="playlistCreationInProgress" />
+          <font-awesome-icon icon="spinner" spin v-show="playlistCreationInProgress"></font-awesome-icon>
+        </form>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-    import Vue from 'vue'
+import Vue from 'vue';
 
-    import {library} from '@fortawesome/fontawesome-svg-core'
-    import {faSpinner} from '@fortawesome/free-solid-svg-icons'
-    import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-    library.add(faSpinner)
-    Vue.component('font-awesome-icon', FontAwesomeIcon)
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-    const formDefaults = {
-                 "date": "2010-01-01",
-                 "minYear": 2010
-               }
+library.add(faSpinner);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
-    export default {
+const formDefaults = {
+  date: '2010-03-05',
+  minYear: 2010,
+};
+
+export default {
   name: 'CreateKillerPlaylist',
-        data() {
-           return {
-               accessToken: '',
-               formFields: Object.assign({}, formDefaults),
-               playlistCreationInProgress: false,
-           }
-        },
-    created() {
-        // TODO - what happens if for some reason this route param doesn't exist?
-        var accessToken = this.$route.params.accessToken
+  data() {
+    return {
+      accessToken: '',
+      formFields: { ...formDefaults },
+      playlistCreationInProgress: false,
+      playingDate: new Date().toISOString().substring(0, 10),
+      menu: false,
+    };
+  },
+  created() {
+    // TODO - what happens if for some reason this route param doesn't exist?
+    const { accessToken } = this.$route.params;
 
-        this.accessToken = accessToken
+    this.accessToken = accessToken;
 
-        this.$http.defaults.headers.common['Authorization'] = accessToken
+    this.$http.defaults.headers.common.Authorization = accessToken;
+  },
+  methods: {
+    maximumDate() {
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1; // January is 0!
+      const yyyy = today.getFullYear();
+      if (dd < 10) {
+        dd = `0${dd}`;
+      }
+      if (mm < 10) {
+        mm = `0${mm}`;
+      }
 
+      today = `${yyyy}-${mm}-${dd}`;
+
+      return today;
     },
-    methods: {
-      maximumDate() {
-        var today = new Date();
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-        var yyyy = today.getFullYear();
-        if(dd<10){
-            dd='0'+dd
-        } 
-        if(mm<10){
-            mm='0'+mm
-        } 
+    maximumNumYears() {
+      const currentDate = new Date();
+      const currentYear = currentDate.getFullYear();
 
-        today = yyyy+'-'+mm+'-'+dd;
-
-        return today
+      return currentYear;
     },
-      maximumNumYears() {
-          var currentDate = new Date();
-          const currentYear = currentDate.getFullYear()
+    specificDateformSubmitted() {
+      delete this.formFields.minYear;
+      this.formSubmitted();
+    },
+    minYearFormSubmitted() {
+      delete this.formFields.date;
+      this.formSubmitted();
+    },
+    formSubmitted() {
+      this.playlistCreationInProgress = true;
 
-          return currentYear
-      },
-      specificDateformSubmitted() {
-        delete this.formFields.minYear
-        this.formSubmitted()
-      },
-      minYearFormSubmitted() {
-        delete this.formFields.date
-        this.formSubmitted()
-      },
-        formSubmitted() {
-            this.playlistCreationInProgress = true
-            
-            this.$http.post('/user/playlists', this.formFields)
-                .then(response => {
-                    var playlistID = response.data.id
-                    var dateString = response.data.date
-                  
-                    var scrapeOfficialChartsURL = `/charts/${dateString}`
+      this.$http.post('/user/playlists', this.formFields).then((response) => {
+        const playlistID = response.data.id;
+        const dateString = response.data.date;
 
-                    return this.$http.get(scrapeOfficialChartsURL)
-                        .then(scrapedData => {
-                            var spotifySearchURL = `/search`
+        const scrapeOfficialChartsURL = `/charts/${dateString}`;
 
-                            return this.$http.post(spotifySearchURL, scrapedData.data)
-                                .then(completedData => {
-                                    var addToPlaylistURL = `/playlists/${playlistID}/tracks`
+        return this.$http.get(scrapeOfficialChartsURL).then((scrapedData) => {
+          const spotifySearchURL = '/search';
 
-                                    return this.$http.post(addToPlaylistURL, completedData.data).then(
-                                        () => {
-                                            alert(`Playlist of ${dateString} added to user`)
-                                            this.playlistCreationInProgress = false
-                                            this.formFields = Object.assign({}, formDefaults)
-                                        }
-                                    )
-                                })
-                        })
-                })
-        }
-    }
-}
+          return this.$http
+            .post(spotifySearchURL, scrapedData.data)
+            .then((completedData) => {
+              const addToPlaylistURL = `/playlists/${playlistID}/tracks`;
 
+              return this.$http
+                .post(addToPlaylistURL, completedData.data)
+                .then(() => {
+                  alert(`Playlist of ${dateString} added to user`);
+                  this.playlistCreationInProgress = false;
+                  this.formFields = { ...formDefaults };
+                });
+            });
+        });
+      });
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import axios from 'axios'
+import Vue from 'vue';
+import axios from 'axios';
+import Vuetify from 'vuetify';
+import App from './App';
+import router from './router';
 
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+Vue.prototype.$http.defaults.baseURL = process.env.BASE_URL;
 
-Vue.prototype.$http = axios
-Vue.prototype.$http.defaults.baseURL = process.env.BASE_URL
+Vue.use(Vuetify);
+export default new Vuetify({});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  vuetify: new Vuetify(),
   axios,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
