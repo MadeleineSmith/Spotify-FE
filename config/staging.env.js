3d62c46bf0e6c2752b55ff3056edf3841329c2a3
Madeleine Smith <madeleinesmith222@gmail.com>
2020-03-14 17:56:38 +0000

    Change Spotify Base URL of BE

--- config/staging.env.js
+++ config/staging.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"staging"',
-  BASE_URL: '"http://spotify-staging.2ryi2nghev.eu-west-1.elasticbeanstalk.com"'
+  BASE_URL: '"https://spotify-playlist-generator-be.herokuapp.com"'
 })
